c0b5227fc9a559fb3ae3d99b5f04cfb95d7c5191
YinChaCha <1719110079@qq.com>
2017-03-22 14:00:06 +0800

    new por

--- app/src/main/java/com/example/administrator/textmyapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/textmyapp/MainActivity.java
@@ -6,12 +6,12 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        TextView textView = (TextView) findViewById(R.id.textView);
     }
 }
